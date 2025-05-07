88021df45448971cf82909d10870488017069b2e
isuriChamya <isuriweeraman0714@gmail.com>
2025-05-07 21:55:08 +0530

    update the frontend of post creation

--- BackEnd/src/main/java/com/example/pafbackendversionthree/controllers/UserPostController.java
+++ BackEnd/src/main/java/com/example/pafbackendversionthree/controllers/UserPostController.java
@@ -5,7 +5,7 @@ import com.example.pafbackendversionthree.dtos.UserPostDto;
 import com.example.pafbackendversionthree.services.UserPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
