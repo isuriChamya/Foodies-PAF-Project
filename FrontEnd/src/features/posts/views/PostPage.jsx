582902e1751b3d76c27aae7e306b2bdedd8a0b1a
MeedumiRashmika <148336034+MeedumiRashmika@users.noreply.github.com>
2025-05-13 23:25:31 +0530

    postpage

--- FrontEnd/src/features/posts/views/PostPage.jsx
+++ FrontEnd/src/features/posts/views/PostPage.jsx
@@ -12,47 +12,40 @@ const PostPage = () => {
   const fetchPosts = async () => {
     try {
       const response = await postApi.getAllPosts();
-
       setPosts(response.reverse());
     } catch (error) {
       console.log(error);
     }
   };
+
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await postApi.getAllPosts();
-        setPosts(response.reverse());
-      } catch (error) {
-        console.log(error);
-      }
-    };
     fetchPosts();
   }, []);
+
   return (
-    <div className="flex justify-center items-center">
-      <div className="w-[55vw]">
-      <CreatePostModal onRefresh={fetchPosts} />
-      {selectedPost && (
-        <UpdatePostModal
-          onClose={() => {
-            setSelectedPost();
-            setUpdaetModalOpened(false);
+    <div className="w-full flex justify-center px-4 py-6">
+      <div className="w-full max-w-6xl">
+        <CreatePostModal onRefresh={fetchPosts} />
+        {selectedPost && (
+          <UpdatePostModal
+            onClose={() => {
+              setSelectedPost();
+              setUpdaetModalOpened(false);
+            }}
+            open={updaetModalOpened}
+            selectedPost={selectedPost}
+            onRefresh={fetchPosts}
+          />
+        )}
+        <PostsList
+          posts={posts}
+          onUpdateClicked={(post) => {
+            setSelectedPost(post);
+            setUpdaetModalOpened(true);
           }}
-          open={updaetModalOpened}
-          selectedPost={selectedPost}
-          onRefresh={fetchPosts}
+          onDeleteSuccess={fetchPosts}
         />
-      )}
-      <PostsList
-        posts={posts}
-        onUpdateClicked={(post) => {
-          setSelectedPost(post);
-          setUpdaetModalOpened(true);
-        }}
-        onDeleteSuccess={fetchPosts}
-      />
-    </div>
+      </div>
     </div>
   );
 };
