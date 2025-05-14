ed9f208bb23949f194f746166f5f445bfb902b47
MeedumiRashmika <148336034+MeedumiRashmika@users.noreply.github.com>
2025-05-14 15:41:12 +0530

    colour change in button

--- FrontEnd/src/features/progress/views/ProgressPage.jsx
+++ FrontEnd/src/features/progress/views/ProgressPage.jsx
@@ -249,7 +249,7 @@ const ProgressPage = () => {
               </button>
               <button
                 onClick={handleDeleteConfirmed}
-                className="px-4 py-2 text-sm bg-red-600 text-white rounded hover:bg-red-700"
+                className="px-4 py-2 text-sm bg-blue-600 text-white rounded hover:bg-blue-700"
               >
                 Delete
               </button>
